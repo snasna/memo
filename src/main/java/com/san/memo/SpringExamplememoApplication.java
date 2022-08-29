30e93c21aeb4e479fd0c19b92352a73a0629c2bd
LAPTOP-QSBIPBFB\김재윤 <kimjy9698@naver.com>
2022-08-29 18:31:48 +0900

    김재윤 2022_0829
    메모 첫째날 과정

--- src/main/java/com/san/memo/SpringExamplememoApplication.java
+++ src/main/java/com/san/memo/SpringExamplememoApplication.java
@@ -1,12 +1,10 @@
 package com.san.memo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringExamplememoApplication {
 
 	public static void main(String[] args) {
