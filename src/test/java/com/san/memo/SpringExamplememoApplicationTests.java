f9d452f7b67ecb7509255c14fb3506b429febfc6
LAPTOP-QSBIPBFB\김재윤 <kimjy9698@naver.com>
2022-08-29 18:06:53 +0900

    김재윤 2022-08-29
    메모 첫시작 단계

---
+++ src/test/java/com/san/memo/SpringExamplememoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.san.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamplememoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
